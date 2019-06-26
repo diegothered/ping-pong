1323f18f76f5fd5cb8dbaa94552047ee88beefd2
Diego Rojas <diego.gustavo.rojas@gmail.com>
2019-06-26 12:56:50 -0700

    add to js for loop and start of functions.

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,21 @@
-var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
-
-for (var index = 0; index < numbers.length; index += 1) {
-        if (number % 3 ==== 0) {
-            return true;
+var pingPong = [];
+  for (var i = 1; i <= input ; i++) { //thought about doing i <= input
+    if (i %3 === 0) {
+      pingPong.push("ping");
+    } else if (i %5 === 0) {
+      pingPong.push("pong");
+    } else if (i %15 ===0) {
+      pingPong.push("ping-pong");
+    } else {
+      pingPong.push(i)
+    };
+  };
 
 $(document).ready(function() {
-  $("form#input").submit(function(event) {
-    event.preventDefault();
-    var pingPong  = parseInt($("input#number").val());
-    var result = numbers(number);
-    $("#result").text(result);
-  });
-});
+  $("form#input").submit(funtion(event)); {
+    event.preventDefault(); //added here because of leap-year website exercise
+    var i = parseInt($("input#number").val());
+    var result = pingPong.push(input);
+    $("#result").text(result); //also want result to be placed in unordered list
+  };
+})
