7ca7498584c066d99661014fb8b42e9c67b61241
Diego Rojas <diego.gustavo.rojas@gmail.com>
2019-06-30 14:44:06 -0700

    try to fix issues with scripts.js.

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,22 @@
 var pingPong = [];
-  for (var i = 1; i <= input ; i++) { //thought about doing i <= input
-    if (i %3 === 0) {
+var input = parseInt($("input#number").val()); //added this in order to define input
+  for (var i = 1; i <= input ; i++) {
+     if (i %3 === 0) {
       pingPong.push("ping");
     } else if (i %5 === 0) {
       pingPong.push("pong");
     } else if (i %15 ===0) {
       pingPong.push("ping-pong");
     } else {
-      pingPong.push(i)
+      pingPong.push(i);
     };
   };
 
 $(document).ready(function() {
-  $("form#input").submit(funtion(event)); {
-    event.preventDefault(); //added here because of leap-year website exercise
-    var i = parseInt($("input#number").val());
+  $("form#input").submit(funtion(event); {
+    event.preventDefault();
+    var input = parseInt($("input#number").val());
     var result = pingPong.push(input);
     $("#result").text(result); //also want result to be placed in unordered list
-  };
-})
+  });
+});
