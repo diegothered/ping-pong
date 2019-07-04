b7c7b7c151c0d73a959675f8caa49c28ae4dbdeb
Student <example@example.com>
2019-07-03 19:57:31 -0700

    work in scripts.js in order to return array.

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,36 @@
-var pingPong = [];
-var input = parseInt($("input#number").val()); //added this in order to define input
-  for (var i = 1; i <= input ; i++) {
-     if (i %3 === 0) {
-      pingPong.push("ping");
-    } else if (i %5 === 0) {
-      pingPong.push("pong");
-    } else if (i %15 ===0) {
-      pingPong.push("ping-pong");
-    } else {
-      pingPong.push(i);
-    }; //still not sure when to return
+var calculate = function(number) {
+  var pingPong = [];
+
+  for (var i = 0; i <= number ; i++);
+
+   if (i % 15 === 0) {
+    pingPong.push("ping-pong");
+  } else if (i % 5 === 0) {
+    pingPong.push("pong");
+  } else if (i % 3 ===0) {
+    pingPong.push("ping");
+  } else {
+    pingPong.push(i);
   };
 
+  return pingPong
+};
+
+
+
 $(document).ready(function() {
-  $("form#input").submit(funtion(event) { //syntax error here but not sure why
+
+  $("form#input").submit(function(event) {
     event.preventDefault();
-    var input = parseInt($("input#number").val());
-    var result = pingPong.push(input);
-    $("#result").text(result); //also want result to be placed in unordered list
+    var input = parseInt($("#number").val());
+    var result = calculate(input);
+    console.log(result);
+
+    result.forEach(function(array) {
+      $("#result").show(array);
+    }) ;
+
+
+
   });
 });
